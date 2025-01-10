a08119b69eb370da8cbe560ad0c7c46363ec1be7
Piotr Brudny <pbrudny@gmail.com>
2025-01-10 13:14:31 +0000

    Create empty book and library classes

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('Hello from the Library project')
+# Book class definition
+class Book:
+    pass
+
+# Library class definition
+class Library:
+    pass
+
+
+# creating Book objects
+book1 = Book()
+book2 = Book()
+
+# creating Library objects
+library1 = Library()
+library2 = Library()
