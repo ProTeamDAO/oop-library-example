829230d19ab97e132f64c18e11b522e7932f02c3
Piotr Brudny <pbrudny@gmail.com>
2025-01-10 13:39:57 +0000

    Add attributes to classes

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 # Book class definition
 class Book:
-    pass
-
+    def __init__(self, title, isbn):
+        self.title = title
+        self.isbn = isbn
+    
+    
 # Library class definition
 class Library:
-    pass
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
 
 
 # creating Book objects
-book1 = Book()
-book2 = Book()
+book1 = Book('Lord of the Rings', '435fsdfs')
+book2 = Book('Python for everybody', '1234dsf')
 
 # creating Library objects
-library1 = Library()
-library2 = Library()
+library1 = Library('National Library', 'Warsaw, Grzybowska 12')
+library2 = Library('Super Library', 'Ankara, Kedi 12')
+
+print(book1.title)
